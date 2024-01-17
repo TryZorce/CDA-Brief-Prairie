de8e16c38465e9691b5f54324d7d6b9aedf12529
TryZorce <christopher.moron@gmail.com>
2024-01-17 14:46:30 +0100

    Front Finis

--- symfony/src/Controller/CustomerController.php
+++ symfony/src/Controller/CustomerController.php
@@ -5,15 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CustomerRepository;
 
 class CustomerController extends AbstractController
 {
     #[Route('/app_customer', name: 'app_customer')]
-    public function index(): Response
+    public function index(CustomerRepository $customerRepository): Response
     {
-        // Ajoutez la logique nécessaire pour récupérer et afficher les clients depuis la base de données
-        $customers = []; // Remplacez ceci par la récupération réelle des clients
-
+      $customers = $customerRepository->findAll();
         return $this->render('customer/index.html.twig', [
             'customers' => $customers,
         ]);
--- symfony/src/Controller/MeetController.php
+++ symfony/src/Controller/MeetController.php
@@ -5,17 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\MeetRepository;
 
 class MeetController extends AbstractController
 {
     #[Route('/app_meet', name: 'app_meet')]
-    public function index(): Response
+    public function index(MeetRepository $meetRepository): Response
     {
-        // Ajoutez la logique nécessaire pour récupérer et afficher les rendez-vous depuis la base de données
-        $meetings = []; // Remplacez ceci par la récupération réelle des rendez-vous
-
+        $meets = $meetRepository->findAll();
         return $this->render('meet/index.html.twig', [
-            'meetings' => $meetings,
+            'meets' => $meets,
         ]);
     }
 }

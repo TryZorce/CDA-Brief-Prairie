0296feb835f50d73e397d3a248d8c2c1083dc19a
TryZorce <christopher.moron@gmail.com>
2024-01-23 14:59:07 +0100

    Replace Symfony Error

--- symfony/src/Entity/Pet.php
+++ symfony/src/Entity/Pet.php
@@ -19,9 +19,6 @@ class Pet
     #[ORM\Column(length: 45)]
     private ?string $Name = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $Image = null;
-
     #[ORM\Column(length: 255)]
     private ?string $Sex = null;
 
@@ -55,18 +52,6 @@ class Pet
     public function setName(string $Name): static
     {
         $this->Name = $Name;
-
-        return $this;
-    }
-
-    public function getImage(): ?string
-    {
-        return $this->Image;
-    }
-
-    public function setImage(?string $Image): static
-    {
-        $this->Image = $Image;
 
         return $this;
     }
@@ -140,7 +125,6 @@ class Pet
     public function removeMeet(Meet $meet): static
     {
         if ($this->meets->removeElement($meet)) {
-            // set the owning side to null (unless already changed)
             if ($meet->getPet() === $this) {
                 $meet->setPet(null);
             }
